7fdebcce97f7e26e05a5a9a3239773dcbf84ec5c
Espy Foxx <railfolf@gmail.com>
2026-06-30 17:31:35 -0500

    Delete lua/entities/gmod_wire_expression2/core/custom directory

--- lua/entities/gmod_wire_expression2/core/custom/cl_submeshmaterials.lua
+++
@@ -1,3 +0,0 @@
-E2Helper.Descriptions["e:getSubmeshCount()"] = "Returns the number of submeshes a prop has."
-E2Helper.Descriptions["e:getBodygroupMask()"] = "Returns the prop's current bodygroup mask."
-E2Helper.Descriptions["e:getSubmeshMaterials()"] = "Returns a table of a prop's submeshes and their materials."
