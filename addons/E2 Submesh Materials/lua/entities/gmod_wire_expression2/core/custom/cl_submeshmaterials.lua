0922fd0719d37613e61b454cdd530e4afcb42685
Espy Foxx <railfolf@gmail.com>
2026-06-30 17:32:37 -0500

    ok this should have the right filepaths

---
+++ addons/E2 Submesh Materials/lua/entities/gmod_wire_expression2/core/custom/cl_submeshmaterials.lua
@@ -0,0 +1,3 @@
+E2Helper.Descriptions["e:getSubmeshCount()"] = "Returns the number of submeshes a prop has."
+E2Helper.Descriptions["e:getBodygroupMask()"] = "Returns the prop's current bodygroup mask."
+E2Helper.Descriptions["e:getSubmeshMaterials()"] = "Returns a table of a prop's submeshes and their materials."
